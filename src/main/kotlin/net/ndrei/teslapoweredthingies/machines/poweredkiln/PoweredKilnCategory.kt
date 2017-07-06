d8f3321c0f5d77659ec24245c90702f7b93cfe2e
faceofcat <candreix@gmail.com>
2017-07-06 23:21:09 +0300

    updated powered kiln's textures

--- src/main/kotlin/net/ndrei/teslapoweredthingies/machines/poweredkiln/PoweredKilnCategory.kt
+++ src/main/kotlin/net/ndrei/teslapoweredthingies/machines/poweredkiln/PoweredKilnCategory.kt
@@ -20,11 +20,12 @@ object PoweredKilnCategory
     override fun setRecipe(recipeLayout: IRecipeLayout, recipeWrapper: PoweredKilnRecipeWrapper, ingredients: IIngredients) {
         val stacks = recipeLayout.itemStacks
 
-        stacks.init(0, true, 6, 23)
+        stacks.init(0, true, 6, 6)
         stacks.set(0, ingredients.getInputs(ItemStack::class.java)[0])
 
-        stacks.init(1, false, 30, 23)
+        stacks.init(1, false, 45, 6)
         stacks.set(1, ingredients.getOutputs(ItemStack::class.java)[0])
+        stacks.setBackground(1, this.slotBackground)
     }
 
     class PoweredKilnRecipeWrapper(val recipe: PoweredKilnRecipe)
@@ -39,7 +40,7 @@ object PoweredKilnCategory
     override fun register(registry: IRecipeCategoryRegistration) {
         super.register(registry)
 
-        this.recipeBackground = this.guiHelper.createDrawable(TeslaThingiesMod.JEI_TEXTURES, 124, 0, 124, 66)
+        this.recipeBackground = this.guiHelper.createDrawable(TeslaThingiesMod.JEI_TEXTURES, 0, 0, 124, 66)
     }
 
     override fun register(registry: IModRegistry) {
