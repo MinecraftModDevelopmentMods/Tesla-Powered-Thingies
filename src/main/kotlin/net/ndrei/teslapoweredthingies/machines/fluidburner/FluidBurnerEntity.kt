e75b1222308410913f5b0dd49e89af8ccbe4b9b1
Face of Cat <candreix@gmail.com>
2017-09-17 16:43:05 +0300

    better sync

--- src/main/kotlin/net/ndrei/teslapoweredthingies/machines/fluidburner/FluidBurnerEntity.kt
+++ src/main/kotlin/net/ndrei/teslapoweredthingies/machines/fluidburner/FluidBurnerEntity.kt
@@ -6,9 +6,9 @@ import net.minecraft.item.EnumDyeColor
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.tileentity.TileEntity
+import net.minecraftforge.common.util.Constants
 import net.minecraftforge.fluids.Fluid
 import net.minecraftforge.fluids.FluidRegistry
-import net.minecraftforge.fluids.FluidStack
 import net.minecraftforge.fluids.IFluidTank
 import net.minecraftforge.items.ItemStackHandler
 import net.ndrei.teslacorelib.containers.BasicTeslaContainer
@@ -17,10 +17,7 @@ import net.ndrei.teslacorelib.gui.BasicRenderedGuiPiece
 import net.ndrei.teslacorelib.gui.BasicTeslaGuiContainer
 import net.ndrei.teslacorelib.gui.FluidTankPiece
 import net.ndrei.teslacorelib.gui.IGuiContainerPiece
-import net.ndrei.teslacorelib.inventory.BoundingRectangle
-import net.ndrei.teslacorelib.inventory.ColoredFluidHandler
-import net.ndrei.teslacorelib.inventory.ColoredItemHandler
-import net.ndrei.teslacorelib.inventory.FluidTank
+import net.ndrei.teslacorelib.inventory.*
 import net.ndrei.teslacorelib.utils.FluidUtils
 import net.ndrei.teslapoweredthingies.client.Textures
 import net.ndrei.teslapoweredthingies.gui.FluidBurnerTankPiece
@@ -29,15 +26,39 @@ import net.ndrei.teslapoweredthingies.gui.IMultiTankMachine
 import net.ndrei.teslapoweredthingies.gui.TankInfo
 import net.ndrei.teslapoweredthingies.machines.BaseThingyGenerator
 import net.ndrei.teslapoweredthingies.render.DualTankEntityRenderer
+import java.util.function.Consumer
+import java.util.function.Supplier
 
 /**
  * Created by CF on 2017-06-30.
  */
 class FluidBurnerEntity : BaseThingyGenerator(FluidBurnerEntity::class.java.name.hashCode()), IMultiTankMachine {
-    private lateinit var coolantTank: FluidTank
-    private lateinit var fuelTank: FluidTank
+    private lateinit var coolantTank: IFluidTank
+    private lateinit var fuelTank: IFluidTank
     private lateinit var coolantItems: ItemStackHandler
     private lateinit var fuelItems: ItemStackHandler
+
+    init {
+        super.registerSyncTagPart(SYNC_CURRENT_FUEL, Consumer {
+            this.fuelInUse = if (it.hasKey("fuelInUse", Constants.NBT.TAG_STRING)) {
+                FluidRegistry.getFluid(it.getString("fuelInUse"))
+            } else null
+
+            this.coolantInUse = if (it.hasKey("coolantInUse", Constants.NBT.TAG_STRING)) {
+                FluidRegistry.getFluid(it.getString("coolantInUse"))
+            } else null
+        }, Supplier {
+            NBTTagCompound().also {
+                if (this.fuelInUse != null) {
+                    it.setString("fuelInUse", FluidRegistry.getFluidName(this.fuelInUse))
+                }
+
+                if (this.coolantInUse != null) {
+                    it.setString("coolantInUse", FluidRegistry.getFluidName(this.coolantInUse))
+                }
+            }
+        }, SyncProviderLevel.GUI)
+    }
 
     var coolantInUse: Fluid? = null
         private set
@@ -47,7 +68,7 @@ class FluidBurnerEntity : BaseThingyGenerator(FluidBurnerEntity::class.java.name
     override fun initializeInventories() {
         super.initializeInventories()
 
-        this.coolantItems = ItemStackHandler(2)
+        this.coolantItems = SyncItemHandler(2)
         super.addInventory(object : ColoredItemHandler(this.coolantItems, EnumDyeColor.MAGENTA, "Coolant Containers", BoundingRectangle(61, 25, FluidTankPiece.WIDTH, FluidTankPiece.HEIGHT)) {
             override fun canInsertItem(slot: Int, stack: ItemStack): Boolean {
                 return slot == 0 && FluidUtils.canFillFrom(this@FluidBurnerEntity.coolantTank, stack)
@@ -79,50 +100,54 @@ class FluidBurnerEntity : BaseThingyGenerator(FluidBurnerEntity::class.java.name
         })
         super.addInventoryToStorage(this.coolantItems, "inv_coolant")
 
-        this.coolantTank = object: FluidTank(5000) {
-            override fun canFillFluidType(fluid: FluidStack?)
-                    = if (fluid != null) FluidBurnerRecipes.isCoolant(fluid) else false
-
-            override fun onContentsChanged() {
-                this@FluidBurnerEntity.markDirty()
-            }
-        }
-        super.addFluidTank(
-                object : ColoredFluidHandler(this.coolantTank,
-                        EnumDyeColor.BLUE,
-                        "Coolant Tank",
-                        BoundingRectangle(79, 25, FluidTankPiece.WIDTH, FluidTankPiece.HEIGHT)) {
-//                    override fun acceptsFluid(fluid: FluidStack): Boolean {
-//                        return FluidBurnerRecipes.isCoolant(fluid)
-//                    }
+//        this.coolantTank = object: FluidTank(5000) {
+//            override fun canFillFluidType(fluid: FluidStack?)
+//                    = if (fluid != null) FluidBurnerRecipes.isCoolant(fluid) else false
 //
-                    override fun canDrain() = false
-                }, null
-        )
+//            override fun onContentsChanged() {
+//                this@FluidBurnerEntity.markDirty()
+//            }
+//        }
+//        super.addFluidTank(
+//                object : ColoredFluidHandler(this.coolantTank,
+//                        EnumDyeColor.BLUE,
+//                        "Coolant Tank",
+//                        BoundingRectangle(79, 25, FluidTankPiece.WIDTH, FluidTankPiece.HEIGHT)) {
+////                    override fun acceptsFluid(fluid: FluidStack): Boolean {
+////                        return FluidBurnerRecipes.isCoolant(fluid)
+////                    }
+////
+//                    override fun canDrain() = false
+//                }, null
+//        )
+        this.coolantTank = this.addSimpleFluidTank(5000, "Coolant Tank", EnumDyeColor.BLUE,
+            79, 25, FluidTankType.INPUT, { FluidBurnerRecipes.isCoolant(it) })
 
-        this.fuelTank = object: FluidTank(5000) {
-            override fun canFillFluidType(fluid: FluidStack?)
-                    = if (fluid != null) FluidBurnerRecipes.isFuel(fluid) else false
+//        this.fuelTank = object: FluidTank(5000) {
+//            override fun canFillFluidType(fluid: FluidStack?)
+//                    = if (fluid != null) FluidBurnerRecipes.isFuel(fluid) else false
+//
+//            override fun onContentsChanged() {
+//                this@FluidBurnerEntity.markDirty()
+//            }
+//
+//        }
+//        super.addFluidTank(
+//                object : ColoredFluidHandler(this.fuelTank,
+//                        EnumDyeColor.RED,
+//                        "Fuel Tank",
+//                        BoundingRectangle(97, 25, FluidTankPiece.WIDTH, FluidTankPiece.HEIGHT)) {
+////                    override fun acceptsFluid(fluid: FluidStack): Boolean {
+////                        return FluidBurnerRecipes.isFuel(fluid)
+////                    }
+//
+//                    override fun canDrain() = false
+//                }, null
+//        )
+        this.fuelTank = this.addSimpleFluidTank(5000, "Fuel Tank", EnumDyeColor.RED,
+            97, 25, FluidTankType.INPUT, { FluidBurnerRecipes.isFuel(it) })
 
-            override fun onContentsChanged() {
-                this@FluidBurnerEntity.markDirty()
-            }
-
-        }
-        super.addFluidTank(
-                object : ColoredFluidHandler(this.fuelTank,
-                        EnumDyeColor.RED,
-                        "Fuel Tank",
-                        BoundingRectangle(97, 25, FluidTankPiece.WIDTH, FluidTankPiece.HEIGHT)) {
-//                    override fun acceptsFluid(fluid: FluidStack): Boolean {
-//                        return FluidBurnerRecipes.isFuel(fluid)
-//                    }
-
-                    override fun canDrain() = false
-                }, null
-        )
-
-        this.fuelItems = ItemStackHandler(2)
+        this.fuelItems = SyncItemHandler(2)
         super.addInventory(object : ColoredItemHandler(this.fuelItems, EnumDyeColor.PURPLE, "Fuel Containers", BoundingRectangle(115, 25, FluidTankPiece.WIDTH, FluidTankPiece.HEIGHT)) {
             override fun canInsertItem(slot: Int, stack: ItemStack): Boolean {
                 return slot == 0 && FluidUtils.canFillFrom(this@FluidBurnerEntity.fuelTank, stack)
@@ -195,11 +220,18 @@ class FluidBurnerEntity : BaseThingyGenerator(FluidBurnerEntity::class.java.name
                 this.coolantInUse = coolant.coolant.fluid
             }
 
+            this.partialSync(SYNC_CURRENT_FUEL)
             power *= this.energyFillRate
             return power
         }
 
         return 0
+    }
+
+    override fun fuelConsumed() {
+        this.fuelInUse = null
+        this.coolantInUse = null
+        this.partialSync(SYNC_CURRENT_FUEL)
     }
 
     override val energyOutputRate: Long
@@ -224,32 +256,32 @@ class FluidBurnerEntity : BaseThingyGenerator(FluidBurnerEntity::class.java.name
         return list
     }
 
-    override fun writeToNBT(compound: NBTTagCompound): NBTTagCompound {
-        var compound = compound
-        compound = super.writeToNBT(compound)
+//    override fun writeToNBT(compound: NBTTagCompound): NBTTagCompound {
+//        var compound = compound
+//        compound = super.writeToNBT(compound)
+//
+//        if (this.fuelInUse != null) {
+//            compound.setString("fuelInUse", FluidRegistry.getFluidName(this.fuelInUse))
+//        }
+//
+//        if (this.coolantInUse != null) {
+//            compound.setString("coolantInUse", FluidRegistry.getFluidName(this.coolantInUse))
+//        }
+//
+//        return compound
+//    }
 
-        if (this.fuelInUse != null) {
-            compound.setString("fuelInUse", FluidRegistry.getFluidName(this.fuelInUse))
-        }
-
-        if (this.coolantInUse != null) {
-            compound.setString("coolantInUse", FluidRegistry.getFluidName(this.coolantInUse))
-        }
-
-        return compound
-    }
-
-    override fun readFromNBT(compound: NBTTagCompound) {
-        super.readFromNBT(compound)
-
-        if (compound.hasKey("fuelInUse")) {
-            this.fuelInUse = FluidRegistry.getFluid(compound.getString("fuelInUse"))
-        }
-
-        if (compound.hasKey("coolantInUse")) {
-            this.coolantInUse = FluidRegistry.getFluid(compound.getString("coolantInUse"))
-        }
-    }
+//    override fun readFromNBT(compound: NBTTagCompound) {
+//        super.readFromNBT(compound)
+//
+//        if (compound.hasKey("fuelInUse")) {
+//            this.fuelInUse = FluidRegistry.getFluid(compound.getString("fuelInUse"))
+//        }
+//
+//        if (compound.hasKey("coolantInUse")) {
+//            this.coolantInUse = FluidRegistry.getFluid(compound.getString("coolantInUse"))
+//        }
+//    }
 
     override fun getTanks()
         = listOf(
@@ -269,4 +301,8 @@ class FluidBurnerEntity : BaseThingyGenerator(FluidBurnerEntity::class.java.name
 //
 //    override val rightTankFluid: Fluid?
 //        get() = this.fuelTank.fluid?.fluid
+
+    companion object {
+        const val SYNC_CURRENT_FUEL = "current_fuel"
+    }
 }
