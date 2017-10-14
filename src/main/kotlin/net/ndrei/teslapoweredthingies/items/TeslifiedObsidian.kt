52b4ffcf8a65b4697bb3ecc521d88feec70c23ac
faceofcat <candreix@gmail.com>
2017-10-14 19:42:14 +0300

    added teslified obsidian item

---
+++ src/main/kotlin/net/ndrei/teslapoweredthingies/items/TeslifiedObsidian.kt
@@ -0,0 +1,6 @@
+package net.ndrei.teslapoweredthingies.items
+
+import net.ndrei.teslacorelib.annotations.AutoRegisterItem
+
+@AutoRegisterItem
+object TeslifiedObsidian : BaseThingyItem("teslified_obsidian")
